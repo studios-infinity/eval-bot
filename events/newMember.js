8ec598cc954a83ba971c45b17c427391aa66610d
Apexin <48616020+shottei@users.noreply.github.com>
2020-08-24 05:08:01 +0300

    Delete newMember.js

--- events/newMember.js
+++
@@ -1,6 +0,0 @@
-module.exports = (client, message) => {
-    client.on('guildMemberAdd', (message) => {
-        let welcomeChannel = client.channels.get('742989518604337204')
-        welcomeChannel.send(`Welcome **${message.member.username}** to the server!`)
-    })
-}
